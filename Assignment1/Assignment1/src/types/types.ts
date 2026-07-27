92f8ea4f67878600de39a286040b935059fb6cbc
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-07-27 15:44:38 +0500

    feat:Assignment1

---
+++ Assignment1/Assignment1/src/types/types.ts
@@ -0,0 +1,23 @@
+export type User = {
+  id: number;
+  name: string;
+};
+
+export type BadgeProps = {
+  text: string;
+  color: string;
+};
+
+export type AvatarProps = {
+  name: string;
+  imageUrl: string;
+};
+
+export type StatCardProps = {
+  title: string;
+  value: number;
+};
+
+export type UserListProps = {
+  users: User[];
+};
