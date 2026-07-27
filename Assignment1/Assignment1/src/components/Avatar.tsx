333841f1e968abbc828ae873224ced68a1beefd4
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-07-27 16:56:49 +0500

    feat:P1 task done with deeper

--- Assignment1/Assignment1/src/components/Avatar.tsx
+++ Assignment1/Assignment1/src/components/Avatar.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import type { AvatarProps } from '../types/types';
 
- const Avatar: React.FC<AvatarProps> = ({ name, imageUrl }) => (
+ const Avatar: React.FC<AvatarProps> = ({ name, imageUrl }) =>{
+   const initials = (name: string): string => {
+    //deeper
+  if (!name.trim()) return "";
+   //deeper
+  const words = name.trim().split(" ");
+  const first = words[0][0] || "";
+  const second = words[1]?.[0] || "";
+
+  return (first + second).toUpperCase();
+};
+const UpdatedName=initials(name);
+const sample=initials("cher");
+console.log("P1 task sample result for cher:",sample);
+console.log("P1 task sample result for Abu Hurairah:",UpdatedName);
+  return (
   <div className="profile-section">
     <div className="avatar-container">
       <div className="avatar-frame">
@@ -9,9 +24,10 @@ import type { AvatarProps } from '../types/types';
       </div>
       
     </div>
-    <h2 className="user-name">{name}</h2>
+    <h2 className="user-name">{UpdatedName}</h2>
     <p className="user-role">Administrator</p>
   </div>
 );
+}
 
 export default Avatar;
