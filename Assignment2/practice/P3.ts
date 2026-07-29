79b1cfb7aa5a6e61a869a1a3e2dda76b6edfa5fd
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-07-29 12:28:20 +0500

    fix:P3 task done with deeper(Applied Ammendments)

--- Assignment2/practice/P3.ts
+++ Assignment2/practice/P3.ts
@@ -4,4 +4,6 @@ export function removeAt(tasks:Task[], id:string) {
   //deeper
   return filtered.length === tasks.length ? tasks : filtered;
 }
+//check
+console.log("P3 task done with deeper",removeAt([{id:'1'},{id:'2'}] as Task[],'1').length === 1)
 //we will not return new array if the id is not found as new array will cause react to re-render.we will return same array.
